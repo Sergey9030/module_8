4eedf4faed46fa3bd953ea256ebedfa4b6c1c727
Sergey9030 <709030@mail.ru>
2024-08-12 22:04:13 +0300

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -2,12 +2,8 @@ def add_everything_up(a, b):
     try:
         return a + b
     except :
-        return (f'{a}+{b}')
+        return (f'{a}{b}')
 
-print(add_everything_up(1, 2))
-print(add_everything_up(1, '2'))
-print(add_everything_up(1, '2345'))
-print(add_everything_up(1, [2, 3, 4, 5]))
-print(add_everything_up(1, (2, 3, 4, 5)))
-print(add_everything_up(1, {2, 3, 4, 5}))
-print(add_everything_up(1, {2: 1, 3: 1, 4: 1, 5: 1}))
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
